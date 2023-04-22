c814acae1397b1e7253a840857d0821c765bd31f
rodinaelframawy <128902223+rodinaelframawy@users.noreply.github.com>
2023-04-22 00:40:29 +0000

    task 4

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,14 +6,12 @@
  * followed by a new line
  * Return: always 0
  */
-
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
-
 	printf("\n");
-return (0);
+        return (0);
 }
