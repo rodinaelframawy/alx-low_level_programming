1a6be3a63ca7e75f5877b0882f2f2ef77b2248e4
rodinaelframawy <128902223+rodinaelframawy@users.noreply.github.com>
2023-04-26 14:16:17 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -  prints the alphabet, in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+char letter;
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('/n');
+}
+int main(void)
+{
+	print_alphabet(void);
+	return 0;
+}
