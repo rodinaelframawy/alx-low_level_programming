779b01b86aaedf3bc2e5682fdcb59569232d9549
rodinaelframawy <128902223+rodinaelframawy@users.noreply.github.com>
2023-04-26 14:37:15 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ * Return: 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++);
+}
+return 0;
+}
