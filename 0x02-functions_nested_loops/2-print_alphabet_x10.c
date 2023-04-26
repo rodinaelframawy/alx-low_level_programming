509323725f7eb60ee275b8ed34a704bc0e2e7775
rodinaelframawy <128902223+rodinaelframawy@users.noreply.github.com>
2023-04-26 15:46:20 +0000

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
-	for (i = 0; i < 10; i++);
-{
+	int count = 0;
 	char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-_putchar(letter);
-}
-_putchar('\n');
-}
+
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
