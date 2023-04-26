2ef5bc87f3ece6f220c988ed80e278b6920b0665
rodinaelframawy <128902223+rodinaelframawy@users.noreply.github.com>
2023-04-26 14:18:15 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,9 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet(void);
-char letter;
 {
+char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('/n');
-}
-int main(void)
-{
-	print_alphabet(void);
-	return 0;
 }
